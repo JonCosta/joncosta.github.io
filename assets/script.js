a05318426f9c2225b86f882255880c8ec90e81f3
JonCosta <jonjcosta@gmail.com>
2017-05-26 14:50:28 -0300

    - Added smooth scrolling to navigation - Fixed navigation offset

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-  
+
   $(".navbar-nav li").click(function() {
     $(".navbar-nav li").removeClass("active");
     $(this).addClass("active");
+  });
+
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Determine element to change to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name="'+ this.hash.slice(1) +'"]');
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000, function() {
+          // Callback after animation, must change focus
+          var $target = $(target);
+          // Checking if target was focused
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            // Adding tabindex for elements not focusable
+            $target.attr('tabindex', '-1');
+          }
+        });
+      }
+    }
   });
   
 });
