7196256f32018141574e80310ae49504f80874a8
Jonathan Costa <jonjcosta@gmail.com>
2024-07-05 14:43:29 -0300

    feat: Layout rework
    Included NPM dependencies and Tailwind

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,55 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
+    const userLang = navigator.language || navigator.userLanguage;
+    // Exemplo: 'en-US' -> 'en'
+    const lang = userLang.split('-')[0];
 
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-
-    $(".navbar-nav li").click(function () {
-        $(".navbar-nav li").removeClass("active");
-        $(this).addClass("active");
-    });
-
-    $('.my-form').on('submit', function (event) {
-        event.preventDefault();
-    });
-
-    $('a[href*="#"]')
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function (event) {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                // Determine element to change to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name="' + this.hash.slice(1) + '"]');
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 100
-                    }, 1000, function () {
-                        // Callback after animation, must change focus
-                        var $target = $(target);
-                        // Checking if target was focused
-                        if ($target.is(":focus")) {
-                            return false;
-                        } else {
-                            // Adding tabindex for elements not focusable
-                            $target.attr('tabindex', '-1');
-                        }
+    // Função para carregar o JSON de tradução
+    function loadTranslation(lang) {
+        fetch(`./assets/locales/${lang}.json`)
+            .then(response => response.json())
+            .then(translations => {
+                applyTranslations(translations);
+            })
+            .catch(() => {
+                console.error(`Translations for ${lang} not found, falling back to English.`);
+                fetch(`./assets/locales/en.json`)
+                    .then(response => response.json())
+                    .then(translations => {
+                        applyTranslations(translations);
                     });
-                }
-            }
-        });
-
-    function setCurrentYear() {
-        var currentYear = new Date().getFullYear();
-        $('#current-year').text(currentYear);
+            });
     }
 
-    setCurrentYear();
+    // Função para aplicar as traduções ao DOM
+    function applyTranslations(translations) {
+        document.querySelectorAll('[data-i18n]').forEach(element => {
+            const key = element.getAttribute('data-i18n');
+            const translatedLabel = getTranslatedLabelFromKeyList(translations, key);
+            if (translatedLabel) {
+                element.innerText = translatedLabel;
+            }
+        });
+    }
+
+    function getTranslatedLabelFromKeyList(translation, key) {
+        const keyList = key.split(".");
+        var translatedLabel = null;
+        var nestedElement = null;
+
+        for (var i = 0; i < keyList.length; i++) {
+            var key = keyList[i];
+            var elementToCheck = nestedElement === null ? translation : nestedElement;
+            if (typeof elementToCheck[key] === 'object') {
+                nestedElement = elementToCheck[key];
+            }
+
+            if (typeof elementToCheck[key] === 'string') {
+                translatedLabel = elementToCheck[key];
+            }
+        }
+        return translatedLabel;
+    }
+
+    // Carregar a tradução apropriada
+    loadTranslation(lang);
 });
